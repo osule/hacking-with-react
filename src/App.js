7ec3fb5dcd1753aeaa0d30e6b9ceb0ca6caa7dd8
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-05-02 06:09:29 +0100

    fetching ajax data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Detail from './pages/Detail';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Detail />
+    );
+  }
+}
+
+
+export default App;
